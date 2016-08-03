18b9e49743cb314fd98c66edf6aa4b12c6687e60
Carl Rude <filyovscky@macbook-pro.local>
2016-08-03 14:23:42 +0300

    14:23 Commit test

---
+++ FriendBookPlus/FriendListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  FriendBookPlus
+//
+//  Created by Yakov Filyovsky on 03/08/16.
+//  Copyright © 2016 Yakov Filyovsky. All rights reserved.
+//
+
+import UIKit
+
+class FriendListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
